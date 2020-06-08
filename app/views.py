9072e3627a78a6c95f77a209152b8b6cd896bb1c
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-08 23:34:48 +0300

    Added a template and undated the index view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
+from flask import render_template
 from app import flask_microblog
 
 
 @flask_microblog.route("/")
 @flask_microblog.route("/index")
 def index():
-    return "Hello, world!"
+    current_user = {"nickname": "Alexander"}
+    posts = [{
+            "author": {"nickname": "Ivan"},
+            "body": "There are a lot of signs in SPB"
+        }, {
+            "author": {"nickname": "Obi Wan"},
+            "body": "Hello there!"
+        }]
+    return render_template("index.html", title="Home", user=current_user, posts=posts)
+    

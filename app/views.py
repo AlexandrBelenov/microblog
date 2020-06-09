5b96becb865d40b810f09b01a757a064c9cd746b
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-09 17:19:57 +0300

    Added openid providers on template

--- app/views.py
+++ app/views.py
@@ -2,13 +2,18 @@ from flask import render_template, flash, redirect
 from app import flask_microblog
 from app.forms import LoginForm
 
+
 @flask_microblog.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash("Login requested for OpenID='{}', remember me= {}".format(form.openid.data, str(form.remember_me.data)))
         return redirect("/index")
-    return render_template("login.html", title="Sign In", form=form)
+    return render_template("login.html",
+        title="Sign In",
+        form=form,
+        providers=flask_microblog.config['OPENID_PROVIDERS'])
+
 
 @flask_microblog.route("/")
 @flask_microblog.route("/index")

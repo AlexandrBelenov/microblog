a5ec0fff72da91125a15445162f3d4086fb8ce02
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-09 03:08:12 +0300

    Added config file

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 # Creating of the Flask application object
 flask_microblog = Flask(__name__)
+flask_microblog.config.from_object('config')
 
 from app import views
---
+++ config.py
@@ -0,0 +1,2 @@
+CSRF_ENABLED = True
+SECRET_KEY = "Input your password"

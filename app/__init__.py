5fe0023ed9e9ab04c2da55aa1aee364e7038850f
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-15 19:25:24 +0300

    Added  db model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # Creating of the Flask application object
 flask_microblog = Flask(__name__)
 flask_microblog.config.from_object('config')
+db = SQLAlchemy(flask_microblog)
 
-from app import views
+
+from app import views, models

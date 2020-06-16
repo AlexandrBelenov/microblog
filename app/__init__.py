45a94d299bb62a792cb78d73cc340ed80db7bb02
Alexander Belenov <AlexanderBelenov@github.com>
2020-06-16 05:05:39 +0300

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 
 # Creating of the Flask application object
 flask_microblog = Flask(__name__)
 flask_microblog.config.from_object('config')
 db = SQLAlchemy(flask_microblog)
 
-
-from app import views, models
+lm = LoginManager()
+lm.init_app(flask_microblog)
+lm.login_view = 'login'
+oid = OpenID(flask_microblog, os.path.join(basedir, 'tmp'))
+from app import models, views
